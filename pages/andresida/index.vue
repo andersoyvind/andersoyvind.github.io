74cdaabe235b714e081df66a2d59440f080d4676
Anders Øyvind Urke-Sætre <anders.urke-satre@vard.com>
2024-06-10 21:32:51 +0200

    It works to some degree, now?

--- pages/andresida/index.vue
+++ pages/andresida/index.vue
@@ -1,29 +1,35 @@
 <script setup>
- import json from "/static/syllabus.json";
- import { ref, onMounted } from 'vue';
- const selectedItem = ref(null);
- const items = ref({});
+import json from "/static/syllabus.json";
+import { ref, onMounted } from "vue";
+const selectedItem = ref(null);
+const items = ref([]);
 
- interface Item {
-   id: number;
-   name: string;
- }
+onMounted(() => {
+  items.value = json.kyus;
+  selectItem(10);
+});
 
- onMounted(() => {
-   items.value = json;
-   console.log(json);
- });
-const selectItem = (itemId: number) => {
-   selectedItem.value = items.value.kyus.find(item => item.id=itemId) || null;
-}
+const selectItem = (itemId) => {
+  console.log("Selected " + itemId);
+  selectedItem.value = items.value.find((item) => item.id === itemId) || null;
+};
 </script>
 <template>
-  <p>Her er andre sida:</p>
-  <div v-if="items.value">
-    <Button :label="kuy.id.toString()" v-for="kuy in items.value.kyus" @click="selectItem(kuy.id)" :key="kuy.id"></Button>
+  <div :key="kuy.id" v-for="kuy in items">
+    <Button
+      :label="kuy.id.toString()"
+      @click="() => selectItem(kuy.id)"
+    ></Button>
   </div>
-  <div v-else>Forresten</div>
-  <div> {{ selectedItem }}</div>
-
+  <div v-if="selectedItem">
+    <h2>Tsuki</h2>
+    <p v-for="tsuki in selectedItem.tsukis">{{tsuki.name}}</p>
+    <h2>Uke</h2>
+    <p v-for="uke in selectedItem.ukes">{{uke.name}}</p>
+    <h2>Keri</h2>
+    <p v-for="keri in selectedItem.keris">{{keri.name}}</p>
+    <h2>Katas</h2>
+    <p v-for="kata in selectedItem.katas">{{ kata.name }}</p>
+  </div>
   <NuxtLink to="/">Tilbake til start</NuxtLink>
 </template>
