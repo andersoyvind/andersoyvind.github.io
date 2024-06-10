74a8218f1cb04c79edbef812d422db07adab855e
Anders Øyvind Urke-Sætre <anders.urke-satre@vard.com>
2024-06-10 20:43:00 +0200

    WIP

--- pages/andresida/index.vue
+++ pages/andresida/index.vue
@@ -1,11 +1,29 @@
 <script setup>
-import json from "/static/syllabus.json";
-import Button from 'primevue/button';
+ import json from "/static/syllabus.json";
+ import { ref, onMounted } from 'vue';
+ const selectedItem = ref(null);
+ const items = ref({});
+
+ interface Item {
+   id: number;
+   name: string;
+ }
+
+ onMounted(() => {
+   items.value = json;
+   console.log(json);
+ });
+const selectItem = (itemId: number) => {
+   selectedItem.value = items.value.kyus.find(item => item.id=itemId) || null;
+}
 </script>
 <template>
   <p>Her er andre sida:</p>
-  <div v-if="json">
-    <Button label="kuy.id" v-for="kuy in json.kyus"></Button>
+  <div v-if="items.value">
+    <Button :label="kuy.id.toString()" v-for="kuy in items.value.kyus" @click="selectItem(kuy.id)" :key="kuy.id"></Button>
   </div>
+  <div v-else>Forresten</div>
+  <div> {{ selectedItem }}</div>
+
   <NuxtLink to="/">Tilbake til start</NuxtLink>
 </template>
